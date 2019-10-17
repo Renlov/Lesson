8652d11640c69f640e9653760e0ca3b60325580a
Renlov <a.pimenov95@mail.ru>
2019-10-18 00:52:31 +0300

    Done

--- src/main/java/com/ifmo/lesson1/Test.java
+++
@@ -1,7 +0,0 @@
-package com.ifmo.lesson1;
-
-public class Test {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/com/ifmo/lesson2/SquareEquation.java
+++ src/main/java/com/ifmo/lesson2/SquareEquation.java
@@ -22,8 +22,20 @@ public class SquareEquation {
     Возвращает массив из двух корней или null, если таковых нет.
      */
     public static double[] squareEquationRoots(double a, double b, double c) {
-        // TODO implement
+        double[] myArray = new double[2];
+        double d = Math.pow(b, 2) - 4 * a * c;
+        if (d > 0) {
+            double x = -b + Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+            double y = -b - Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+            System.out.println(x);
+            System.out.println(y);
+        } else if (d == 0) {
+            double x = -b / 2 * a;
+            System.out.println(x);
+        } else {
+            System.out.println("Корней нет");
 
+        }
         return null;
     }
 }
