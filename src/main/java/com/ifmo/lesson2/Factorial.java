40b8f106681fe7df61478d35e24c628dc1d7f904
Renlov <a.pimenov95@mail.ru>
2019-10-20 22:47:55 +0400

    Fix

--- src/main/java/com/ifmo/lesson2/Factorial.java
+++ src/main/java/com/ifmo/lesson2/Factorial.java
@@ -20,7 +20,7 @@ public class Factorial {
     public static long factorial(int n) {
         // TODO implement
         int a = 1;
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
         a*=i;
         }
         return a;
