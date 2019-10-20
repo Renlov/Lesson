3d0c4646ab4c8894220cfd465817386ebe41abcc
Renlov <a.pimenov95@mail.ru>
2019-10-20 22:59:01 +0400

    Fix

--- src/main/java/com/ifmo/lesson2/First20.java
+++ src/main/java/com/ifmo/lesson2/First20.java
@@ -9,7 +9,7 @@ public class First20 {
         int b = 1;
         for (int i = 0; i <20 ; i++) {
             b*=2;
-                    System.out.println(b);
+                    System.out.print(b+" ");
         }
         }
     }
