fdfde77b7132db6deed4bc5e4238fba186d7edc1
Renlov <a.pimenov95@mail.ru>
2019-10-20 22:29:54 +0400

    Fix

--- src/main/java/com/ifmo/lesson2/ClosestToTen.java
+++ src/main/java/com/ifmo/lesson2/ClosestToTen.java
@@ -6,18 +6,17 @@ public class ClosestToTen {
      переменные m и n. Например, среди чисел 8,5 и 11,45 ближайшее к десяти 11,45.
      */
     public static void main(String[] args) {
-        float m = 8.5f;
         float n = 11.45f;
-
+        float m = 8.5f;
         float closestToTen = closestToTen(m, n);
-
         System.out.println(closestToTen);
     }
 
     public static float closestToTen(float m, float n) {
         // TODO implement
-        float a = m-n;
-        float b = n-m;
+        float ten = 10.0f;
+        float a = Math.abs(ten - n);
+        float b =  Math.abs(ten - m);
         if (a > b) return m;
         else return n;
     }
