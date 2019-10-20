7fe3b8dfc8ec0ca9822893b6e3a692d6c2402937
Renlov <a.pimenov95@mail.ru>
2019-10-20 14:36:45 +0400

    Done

--- src/main/java/com/ifmo/lesson3/Random8.java
+++ src/main/java/com/ifmo/lesson3/Random8.java
@@ -1,5 +1,8 @@
 package com.ifmo.lesson3;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Random8 {
     /*
     Создайте массив из 8 случайных целых чисел из отрезка [1;10]. Выведите массив на экран
@@ -9,22 +12,31 @@ public class Random8 {
     public static void main(String[] args) {
         int[] randomNumbers = randomNumbers();
 
-        // TODO implement
+        System.out.println(Arrays.toString(randomNumbers));
 
         int[] replacedWithZeros = replaceWithZeros(randomNumbers);
 
-        // TODO implement
+        System.out.println(Arrays.toString(replacedWithZeros));
     }
 
     public static int[] randomNumbers() {
-        // TODO implement
-
-        return new int[0];
+        Random rnd = new Random();
+        int low = 1, high = 10;
+        int[] arr = new int[8];
+        for (int i = 0; i <arr.length; i++) {
+            int j = rnd.nextInt(high - low) + low;
+            arr[i] = j;
+        }
+        return arr;
     }
 
     public static int[] replaceWithZeros(int[] randomNumbers) {
-        // TODO implement
+        int[] arr = new int[8];
+        for (int i = 0; i <randomNumbers.length ; i++) {
+            if (randomNumbers[i] % 2 == 0) arr[i] = randomNumbers[i];
+            else arr[i] = 0;
+        }
 
-        return new int[0];
+        return arr;
     }
 }
