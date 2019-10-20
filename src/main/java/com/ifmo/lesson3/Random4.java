baed5daabcfc50fdf5e6d24a715be9681135b986
Renlov <a.pimenov95@mail.ru>
2019-10-20 14:20:46 +0400

    Done

--- src/main/java/com/ifmo/lesson3/Random4.java
+++ src/main/java/com/ifmo/lesson3/Random4.java
@@ -1,5 +1,10 @@
 package com.ifmo.lesson3;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Random4 {
     /*
     Создайте массив из 4 случайных целых чисел из отрезка [10;99], выведите его на экран в
@@ -8,19 +13,23 @@ public class Random4 {
      */
     public static void main(String[] args) {
         int[] randomNumbers = randomNumbers();
-
-        // TODO implement
+        System.out.println(Arrays.toString(randomNumbers));
+        System.out.println(isIncreasingSequence(randomNumbers));
     }
 
     public static int[] randomNumbers() {
-        // TODO implement
-
-        return new int[0];
+        Random rnd = new Random();
+        int low = 10, high = 99;
+        int[] arr = new int[4];
+        for (int i = 0; i <arr.length; i++) {
+            int j = rnd.nextInt(high - low) + low;
+            arr[i] = j;
+        }
+        return arr;
     }
 
     public static boolean isIncreasingSequence(int[] randomNumbers) {
-        // TODO implement
-
-        return false;
+        if(randomNumbers[0] < randomNumbers[1] && randomNumbers[1] < randomNumbers[2] && randomNumbers[2] < randomNumbers[3]) return true;
+        else return false;
     }
 }
