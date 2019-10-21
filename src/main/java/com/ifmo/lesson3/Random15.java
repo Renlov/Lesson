5befc9aeb0188b23a54d8a1e0af363ecec2e1c72
Renlov <a.pimenov95@mail.ru>
2019-10-21 22:22:38 +0400

    Done

--- src/main/java/com/ifmo/lesson3/Random15.java
+++ src/main/java/com/ifmo/lesson3/Random15.java
@@ -1,5 +1,8 @@
 package com.ifmo.lesson3;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Random15 {
     /*
      Создайте массив из 15 случайных целых чисел из отрезка [0;9]. Выведите массив на экран.
@@ -8,23 +11,31 @@ public class Random15 {
      */
     public static void main(String[] args) {
         int[] randomNumbers = randomNumbers();
-
+        System.out.println(Arrays.toString(randomNumbers));
         // TODO implement
-
         int evens = evens(randomNumbers);
-
+        System.out.println(evens);
         // TODO implement
     }
 
     public static int[] randomNumbers() {
-        // TODO implement
-
-        return new int[0];
+        Random rnd = new Random();
+        int low = 0, high = 9;
+        int[] arr = new int[15];
+        for (int i = 0; i <arr.length; i++) {
+            int j = rnd.nextInt(high - low) + low;
+            arr[i] = j;
+        }
+        return arr;
     }
 
     private static int evens(int[] arr) {
-        // TODO implement
-
-        return 0;
+        int count = 0;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]%2 == 0){
+                if(arr[i] ==0) continue;
+                else count++; }
+        }
+        return count;
     }
 }
