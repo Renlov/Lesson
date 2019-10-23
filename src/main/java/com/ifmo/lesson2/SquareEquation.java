e57ad294cd93cdb6d7afdf5d0d1a92bc27663c42
Renlov <a.pimenov95@mail.ru>
2019-10-23 20:23:14 +0400

    Fix

--- src/main/java/com/ifmo/lesson2/SquareEquation.java
+++ src/main/java/com/ifmo/lesson2/SquareEquation.java
@@ -22,19 +22,15 @@ public class SquareEquation {
     Возвращает массив из двух корней или null, если таковых нет.
      */
     public static double[] squareEquationRoots(double a, double b, double c) {
-        double[] myArray = new double[2];
-        double d = Math.pow(b, 2) - 4 * a * c;
-        if (d > 0) {
-            double x = ((-1*b) + Math.sqrt(d))/(2*a);;
-            double y = ((-1*b) - Math.sqrt(d))/(2*a);;
-            myArray[0] = x;
-            myArray[1] =y;
-        } else if (d == 0) {
-            double x = -b / 2 * a;
-            myArray[0] = x;
-        } else {
-            return null;
+        double discriminant = Math.pow(b, 2) - 4*a*c;
+        if(discriminant > 0){
+            double root1 = ((-b)+Math.sqrt(discriminant))/(2 * a);
+            double root2 = ((-b)-Math.sqrt(discriminant))/(2 * a);
+            return new double[]{root1, root2};
+        }else if(discriminant == 0){
+            double root = (-b)/(2 * a);
+            return new double[]{root};
         }
-        return myArray;
+        return null;
     }
 }
