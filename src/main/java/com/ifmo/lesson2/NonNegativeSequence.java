bec47e064d632b74981b1c5922da7b1581ff9a30
Renlov <a.pimenov95@mail.ru>
2019-10-23 20:24:35 +0400

    Fix

--- src/main/java/com/ifmo/lesson2/NonNegativeSequence.java
+++ src/main/java/com/ifmo/lesson2/NonNegativeSequence.java
@@ -7,7 +7,7 @@ public class NonNegativeSequence {
      */
     public static void main(String[] args) {
         for (int i = 90; i > 0; i-=5) {
-            System.out.print(i + " ");
+            System.out.println(i);
 
         }
     }
