861b6a5c7ea01872fd8df17b5f88f7801c9878be
Renlov <a.pimenov95@mail.ru>
2019-10-27 21:34:52 +0400

    Done

--- src/main/java/com/ifmo/lesson2/PrimeNumber.java
+++ src/main/java/com/ifmo/lesson2/PrimeNumber.java
@@ -24,7 +24,7 @@ public class PrimeNumber {
 
     public static boolean isPrime(int n) {
         if(n <= 1) return false;
-        for (int i = 2; i < Math.sqrt(n); i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if(n % i == 0) return false;
         }
         return true;
