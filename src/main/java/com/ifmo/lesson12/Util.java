382fd556ea11109a19d36bc93b70d03c95caf65c
Renlov <a.pimenov95@mail.ru>
2019-11-12 21:47:38 +0300

    New

--- src/main/java/com/ifmo/lesson12/Util.java
+++ src/main/java/com/ifmo/lesson12/Util.java
@@ -2,11 +2,11 @@ package com.ifmo.lesson12;
 import java.util.*;
 
 
-public interface Util {
+public class Util { //
     interface Predicate<T>{
         boolean isValid(T value);
     }
-    public static <T>List<T> filter(List<T> list, Predicate<T> filter){
+    public static <T> List<T> filter(List<T> list, Predicate<T> filter){
         List<T> result = new ArrayList<>(list.size());
         for (T item: list) {
             if(filter.isValid(item)) result.add(item);
@@ -18,25 +18,21 @@ public interface Util {
     interface Predicate1<T, R>{
         boolean isValue(T value);
     }
-
     interface Transformer<T, R> {
         R transform(T value);
     }
     public static <T, R> List<R> transform(List<T> list, Transformer<T, R> transformer) {
-        List<T> result = new ArrayList<>(list.size());
-
-        return null;
+        List<R> result = new ArrayList<>(list.size());
+        for (T value: list) {
+            result.add(transformer.transform(value));
+        }
+        return result;
     }
-
     public static void main(String[] args) {
         List<Integer> integers = List.of(1,2,3,4,5,6,7,8,9,10);
-
         List<Integer> even = filter(integers, i -> i % 2 == 0);
-
         even.forEach(System.out::println);
-
         System.out.println("============");
-
         filter(integers, i -> i>5).forEach(System.out::println);
         System.out.println("============");
         filter(integers, i -> i>2).forEach(System.out::println);
---
+++ src/main/java/com/ifmo/lesson13/StreamTasks.java
@@ -0,0 +1,115 @@
+package com.ifmo.lesson13;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamTasks {
+
+    private static class Person {
+        private final String name;
+        private final int age;
+        private final String country;
+
+        public Person(String name, int age, String country) {
+            this.name = name;
+            this.age = age;
+            this.country = country;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stream<Person> people = generatePeople(100);
+
+        List<String> countries = countriesSortedByTheirPopulationDescending(people);
+        String countryThatHasMostKids = countryThatHasMostKids(people);
+        Map<String, Integer> populationByCountry = populationByCountry(people);
+
+        System.out.println(countries);
+        System.out.println(countryThatHasMostKids);
+        System.out.println(populationByCountry);
+
+        List<String> words = List.of("the", "hello", "approximation", "greetings", "java", "war");
+
+        Map<Integer, Set<String>> wordsByLength = groupWordsByLength(words);
+        int averageWordLength = averageWordLength(words);
+        Set<String> longestWords = longestWords(words);
+
+        System.out.println(wordsByLength);
+        System.out.println(averageWordLength);
+        System.out.println(longestWords);
+        for (String name: generatePeople(10)
+             ) {
+            System.out.println(name);
+
+        }
+    }
+
+    // Метод возвращает страны в порядке убывания их населения.
+    public static List<String> countriesSortedByTheirPopulationDescending(Stream<Person> people) {
+        // TODO implement.
+       // Map<String, Person> result =
+        return List.of();
+    }
+
+    // Метод находит страну (или одну из стран), в которых больше всего человек в возрасте
+    // до 18 лет.
+    public static String countryThatHasMostKids(Stream<Person> people) {
+        // TODO implement.
+        return  people
+                .filter(p -> p.age < 18)
+                .collect(Collectors.groupingBy(p ->p.country, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparingLong(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
+                .orElse("Unknown");
+    }
+
+    // Метод возвращает карту стран их населения.
+    public static Map<String, Long> populationByCountry(Stream<Person> people) {
+        // TODO implement.
+
+        return Map.of();
+    }
+
+    private static List<String> COUNTRIES = List.of("USA", "Russia", "India");
+    private static List<String> NAMES = List.of("Mike", "Max", "Jon", "Anastasia", "Elizabeth", "Viki");
+    // Метод генерирует случайных людей в ограниченном наборе стран.
+    // number - число желаемых людей.
+    public static Stream<Person> generatePeople(int number) {
+        // TODO implement.
+        var rnd = new Random(80);
+        int rnx = rnd.nextInt();
+        Stream people = Stream.generate(
+                () -> new Person(NAMES.get(rnd.nextInt(NAMES.size())), rnx, COUNTRIES.get(rnd.nextInt(COUNTRIES.size())))
+        );
+        return people;
+    }
+
+    // Метод возвращает карту сгруппированных слов по их длине. Например, для
+    // трёхбуквенных слов будет:
+    // 3 -> "the", "map", "got", "war"...
+    public static Map<Integer, Set<String>> groupWordsByLength(List<String> words) {
+        // TODO implement.
+
+        return Map.of();
+    }
+
+    // Метод находит среднюю длину слов в списке.
+    public static int averageWordLength(List<String> words) {
+        // TODO implement.
+
+        return 0;
+    }
+
+    // Метод находит самое длинное слово или слова, если таких несколько.
+    public static Set<String> longestWords(List<String> words) {
+        // TODO implement.
+
+        return Set.of();
+    }
+
+
+
+}
