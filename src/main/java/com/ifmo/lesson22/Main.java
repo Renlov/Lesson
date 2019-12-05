ccb792ee27d461f32684ed722285c1ee11930b7e
Renlov <a.pimenov95@mail.ru>
2019-12-05 18:42:10 +0300

    New

---
+++ src/main/java/com/ifmo/lesson22/Main.java
@@ -0,0 +1,11 @@
+package com.ifmo.lesson22;
+
+public class Main {
+    public class Producer extends Thread{
+
+    }
+    public static void main(String[] args) {
+        Thread thread = new Thread();
+
+    }
+}
