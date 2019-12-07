862b7d3c05d5fd568175403ab6e75cc818397c30
Renlov <a.pimenov95@mail.ru>
2019-12-07 23:40:19 +0300

    Done test

---
+++ src/main/java/com/ifmo/lesson24/TestAtHome.java
@@ -0,0 +1,27 @@
+package com.ifmo.lesson24;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class TestAtHome {
+    public static CyclicBarrier barrier= new CyclicBarrier(2);
+    public static void main(String[] args) throws InterruptedException {
+        Pool thread1 = new Pool();
+        Pool thread2 = new Pool();
+        thread1.start();
+        Thread.sleep(1000);
+        thread2.start();
+    }
+    public static class Pool extends Thread{
+        @Override
+        public void run(){
+            try {
+                System.out.println("Поток " + Thread.currentThread().getName() + " запустился");
+                barrier.await();
+                System.out.println("Все потоки отработали");
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
