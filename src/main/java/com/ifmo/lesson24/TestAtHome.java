53af21d6544104dc4c2a367c3b4e15b4cb5d67b6
Renlov <a.pimenov95@mail.ru>
2019-12-07 23:46:50 +0300

    Done test

--- src/main/java/com/ifmo/lesson24/TestAtHome.java
+++ src/main/java/com/ifmo/lesson24/TestAtHome.java
@@ -3,13 +3,16 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class TestAtHome {
-    public static CyclicBarrier barrier= new CyclicBarrier(2);
+    public static CyclicBarrier barrier= new CyclicBarrier(3);
     public static void main(String[] args) throws InterruptedException {
         Pool thread1 = new Pool();
         Pool thread2 = new Pool();
+        Pool thread3 = new Pool();
         thread1.start();
         Thread.sleep(1000);
         thread2.start();
+        Thread.sleep(1000);
+        thread3.start();
     }
     public static class Pool extends Thread{
         @Override
@@ -17,7 +20,7 @@ public class TestAtHome {
             try {
                 System.out.println("Поток " + Thread.currentThread().getName() + " запустился");
                 barrier.await();
-                System.out.println("Все потоки отработали");
+                System.out.println("Поток " + Thread.currentThread().getName() + " снова сработал");
             } catch (InterruptedException | BrokenBarrierException e) {
                 e.printStackTrace();
             }
