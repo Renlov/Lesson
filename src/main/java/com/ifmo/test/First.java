ea22e7446826a86414336f70d178f9e05bafcd70
Renlov <a.pimenov95@mail.ru>
2019-12-17 21:24:19 +0300

    Done test

--- src/main/java/com/ifmo/test/First.java
+++ src/main/java/com/ifmo/test/First.java
@@ -2,32 +2,54 @@ package com.ifmo.test;
 
 public class First {
     public static void main(String[] args) throws InterruptedException {
+        Barrier barrier = new Barrier();
+
         for (int i = 0; i <3; i++) {
-            Barrier barrier = new Barrier();
-            barrier.start();
-            barrier.await();
+            new TestThread(barrier).start();
+            Thread.sleep(1000);
+        }
+    }
+
+    static class Barrier {
+        private int i = 0;
+        void await() throws InterruptedException {
+            synchronized (this) {
+
+                if (++i == 3) {
+                    System.out.println("potoki zapucheni");
+                    notifyAll();
+                } else {
+                    wait();
+
+                }
+            }
+        }
+
+    }
+
+    private static class TestThread extends Thread {
+        private final Barrier barrier;
+
+        private TestThread(Barrier barrier) {
+            this.barrier = barrier;
+        }
+
+        @Override
+        public void run(){
+            System.out.println("before barrier");
+            try {
+                barrier.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("After barrier");
+
+//            try {
+//                System.out.println("sozdaem potok " + Thread.currentThread().getName());
+//                Thread.sleep(1000);
+//            } catch (Exception ignored) {}
         }
     }
 }
 
-class Barrier extends Thread {
-    private int i = 0;
-    void await() throws InterruptedException {
-        synchronized (this) {
-            if (i == 3) {
-                System.out.println("potoki zapucheni");
-                notifyAll();
-            } else {
-                i++;
-                wait();
-            }
-        }
-    }
-    @Override
-    public void run(){
-        try {
-            System.out.println("sozdaem potok " + Thread.currentThread().getName());
-            Thread.sleep(1000);
-        } catch (Exception e) {}
-    }
-}
+
